1893bf0ad4d81addf63dad2f2febc7f3dcaf5b51
Arraidh <ari.darmika@gmail.com>
2023-03-21 09:12:26 +0700

    add todo item to console

---
+++ js/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTodo();
+  });
+});
+
+function addTodo() {
+  const todos = [];
+  const RENDER_EVENT = "render-todo";
+
+  const textTodo = document.getElementById("title").value;
+  const timestamp = document.getElementById("date").value;
+
+  const generatedID = generateId();
+  const todoObject = generateTodoObject(
+    generatedID,
+    textTodo,
+    timestamp,
+    false
+  );
+  todos.push(todoObject);
+
+  document.addEventListener(RENDER_EVENT, function () {
+    console.log(todos);
+  });
+
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+//getting current timestamp in js
+function generateId() {
+  return +new Date();
+}
+
+function generateTodoObject(id, task, timestamp, isCompleted) {
+  return {
+    id,
+    task,
+    timestamp,
+    isCompleted,
+  };
+}
